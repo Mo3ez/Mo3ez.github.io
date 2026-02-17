7eb90cb5c6de0db6aceb4b7eee3770d3b05f7136
Mo3ez <abdelmouezamine@gmail.com>
2026-02-17 16:38:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ function showSection(sectionId) {
   sections.forEach(section => {
     if(section.id === sectionId) {
       section.classList.remove('hidden');
+      section.scrollIntoView({behavior: 'smooth'});
     } else {
       section.classList.add('hidden');
     }
   });
+}
+
+// Reset tout
+function resetPortfolio() {
+  const sections = document.querySelectorAll('.content-section');
+  sections.forEach(section => section.classList.add('hidden'));
+  window.scrollTo({top: 0, behavior: 'smooth'});
 }
